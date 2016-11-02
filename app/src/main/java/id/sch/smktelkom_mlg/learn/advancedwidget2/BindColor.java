61780ff08394894173fbef4a5391a6ab7ef46203
Febry Muchammad <mc.ebryl@gmail.com>
2016-11-02 20:28:29 +0700

    Advanced Widget Final Part

---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/advancedwidget2/BindColor.java
@@ -0,0 +1,7 @@
+package id.sch.smktelkom_mlg.learn.advancedwidget2;
+
+/**
+ * Created by Febry Muchammad on 02/11/2016.
+ */
+public @interface BindColor {
+}
---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/advancedwidget2/ButterKnife.java
@@ -0,0 +1,9 @@
+package id.sch.smktelkom_mlg.learn.advancedwidget2;
+
+/**
+ * Created by Febry Muchammad on 02/11/2016.
+ */
+public class ButterKnife {
+    public static void bind(MainActivity mainActivity) {
+    }
+}
